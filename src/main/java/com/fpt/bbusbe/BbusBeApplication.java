a3d79039e11447d8d1e715a3912cc4d61d6b7742
loinq2704 <nguyenquangloi2704@gmail.com>
2025-01-23 10:26:59 +0700

    intialize project

---
+++ src/main/java/com/fpt/bbusbe/BbusBeApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.bbusbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbusBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbusBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/bbusbe/BbusBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.bbusbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbusBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
